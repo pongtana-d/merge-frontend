aa7b010fba2d2ef315fdd6db5f4b9328f8d1c754
Mono-Tong <pongtana@monosus.co.jp>
2024-12-26 02:58:27 +0700

    add: countdown js

--- src/scripts/modules/countdown.ts
+++ src/scripts/modules/countdown.ts
@@ -1,53 +1,44 @@
 import $ from 'jquery';
+import countdown from 'countdown';
 
 export default function countDown() {
   const $el = $('.js-countdown');
 
   if (!$el.length) return;
 
-  const $hourTens = $el.find('.sec-flash-sale__digit--hour-tens');
-  const $hourOnes = $el.find('.sec-flash-sale__digit--hour-ones');
-  const $minTens = $el.find('.sec-flash-sale__digit--min-tens');
-  const $minOnes = $el.find('.sec-flash-sale__digit--min-ones');
-  const $secTens = $el.find('.sec-flash-sale__digit--sec-tens');
-  const $secOnes = $el.find('.sec-flash-sale__digit--sec-ones');
-  let time = Number($el.data('countdown-time'));
+  const endTime = $el.data('end-time');
+  let interval: countdown.Timespan | number;
 
-  if (!time || isNaN(time)) return;
+  if (!endTime) return;
 
-  if (time > 359999) {
-    time = 359999;
-  }
+  const moveDigit = ($target: JQuery<HTMLElement>, units: number, digit: number) => {
+    const y = (units - digit) * -24;
 
-  const hours = Math.floor(time / 3600);
-  const minutes = Math.floor((time % 3600) / 60);
-  const secs = time % 60;
+    if (digit === 0) {
+      setTimeout(() => {
+        $target.css({ transform: `translateY(0px)`, transition: '' });
+      }, 900);
+    }
+    $target.css({ transform: `translateY(${y}px)`, transition: 'transform 0.9s ease-out' });
+  };
 
-  // Pad with leading zeros for single digits
-  const hh = String(hours).padStart(2, '0');
-  const mm = String(minutes).padStart(2, '0');
-  const ss = String(secs).padStart(2, '0');
+  interval = countdown(
+    (ts) => {
+      const hh = String(ts.hours).padStart(2, '0');
+      const mm = String(ts.minutes).padStart(2, '0');
+      const ss = String(ts.seconds).padStart(2, '0');
 
-  console.log(hh, mm, ss);
+      if (ts.value === 0) clearInterval(interval as number);
 
-  const secOfSecOnes = Number(ss.charAt(1));
-  const secOfSecTens = Number(ss.charAt(0)) * 10 + secOfSecOnes;
-  const secOfMinOnes = Number(mm.charAt(1)) * 60 + secOfSecTens;
-  const secOfMinTens = Number(mm.charAt(0)) * 600 + secOfMinOnes;
-  const secOfHourOnes = Number(hh.charAt(1)) * 3600 + secOfMinTens;
-  const secOfHourTens = Number(hh.charAt(0)) * 36000 + secOfHourOnes;
-
-  $secOnes.attr('style', `--delay:${(10 - secOfSecOnes) * -1}s`);
-  $secTens.attr('style', `--delay:${(70 - secOfSecTens) * -1}s`);
-  $minOnes.attr('style', `--delay:${(660 - secOfMinOnes) * -1}s`);
-  $minTens.attr('style', `--delay:${(4200 - secOfMinTens) * -1}s`);
-  $hourOnes.attr('style', `--delay:${(39600 - secOfHourOnes) * -1}s`);
-  $hourTens.attr('style', `--delay:${(396000 - secOfHourTens) * -1}s`);
-
-  setTimeout(
-    () => {
-      // $el.attr('data-stop', 'true');
+      moveDigit($el.find('.js-h-tens'), 10, Number(hh.charAt(0)));
+      moveDigit($el.find('.js-h-ones'), 10, Number(hh.charAt(1)));
+      moveDigit($el.find('.js-m-tens'), 6, Number(mm.charAt(0)));
+      moveDigit($el.find('.js-m-ones'), 10, Number(mm.charAt(1)));
+      moveDigit($el.find('.js-s-tens'), 6, Number(ss.charAt(0)));
+      moveDigit($el.find('.js-s-ones'), 10, Number(ss.charAt(1)));
     },
-    (secOfHourTens - 1.1) * 1000,
+    new Date(endTime),
+
+    countdown.HOURS | countdown.MINUTES | countdown.SECONDS,
   );
 }
