6a302419962ae41c617b468088051201e9f15a98
Mono-Tong <pongtana@monosus.co.jp>
2025-03-11 23:29:33 +0700

    add custom select

--- src/scripts/modules/form.ts
+++ src/scripts/modules/form.ts
@@ -1,4 +1,21 @@
+// @ts-nocheck
 import $ from 'jquery';
+import select2 from 'select2';
+
+select2();
+const Defaults = $.fn.select2.amd.require('select2/defaults');
+$.extend(Defaults.defaults, {
+  searchInputPlaceholder: '',
+});
+
+const SearchDropdown = $.fn.select2.amd.require('select2/dropdown/search');
+const _renderSearchDropdown = SearchDropdown.prototype.render;
+SearchDropdown.prototype.render = function () {
+  // invoke parent method
+  const $rendered = _renderSearchDropdown.apply(this, Array.prototype.slice.apply(arguments));
+  this.$search.attr('placeholder', this.options.get('searchInputPlaceholder'));
+  return $rendered;
+};
 
 const textareaCount = () => {
   $('.js-textarea-count').each(function () {
@@ -14,6 +31,35 @@ const textareaCount = () => {
   });
 };
 
+const customSelect = () => {
+  const formatState = (state) => {
+    if (!state.id) {
+      return state.text;
+    }
+    var baseUrl = '/assets/images/flag/';
+    var $state = $(
+      '<span class="select2-result-flex"><img src="' +
+        baseUrl +
+        state.element.value.toLowerCase() +
+        '.svg" alt="" /> ' +
+        state.text +
+        '</span>',
+    );
+    return $state;
+  };
+
+  $('.js-custom-select').each(function () {
+    $(this).select2({
+      width: '100%',
+      dropdownParent: $(this).parent(),
+      searchInputPlaceholder: 'Search...',
+      templateResult: formatState,
+      templateSelection: formatState,
+    });
+  });
+};
+
 export default function form() {
   textareaCount();
+  customSelect();
 }
