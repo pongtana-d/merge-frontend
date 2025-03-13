e0e02b626ae6e547556df69ba45700d4fefeaa10
Mono-Tong <pongtana@monosus.co.jp>
2025-03-14 00:10:43 +0700

    save

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 import autoprefixer from 'autoprefixer';
+import tailwindcss from '@tailwindcss/postcss';
 
 export default {
-  plugins: [autoprefixer()],
+  plugins: [autoprefixer(), tailwindcss()],
 };
