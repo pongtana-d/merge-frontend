bd51f4d8790fc9dc9dae528293b3799f90444c4b
Mono-Tong <pongtana@monosus.co.jp>
2025-03-17 23:04:51 +0700

    add: login, otp, register

--- src/scripts/modules/form.ts
+++ src/scripts/modules/form.ts
@@ -1,20 +1,20 @@
 // @ts-nocheck
 import $ from 'jquery';
-import select2 from 'select2';
+// import select2 from 'select2';
 
-select2();
-const Defaults = $.fn.select2.amd.require('select2/defaults');
-$.extend(Defaults.defaults, {
-  searchInputPlaceholder: 'Search...',
-});
+// select2();
+// const Defaults = $.fn.select2.amd.require('select2/defaults');
+// $.extend(Defaults.defaults, {
+//   searchInputPlaceholder: 'Search...',
+// });
 
-const SearchDropdown = $.fn.select2.amd.require('select2/dropdown/search');
-const _renderSearchDropdown = SearchDropdown.prototype.render;
-SearchDropdown.prototype.render = function () {
-  const $rendered = _renderSearchDropdown.apply(this, Array.prototype.slice.apply(arguments));
-  this.$search.attr('placeholder', this.options.get('searchInputPlaceholder'));
-  return $rendered;
-};
+// const SearchDropdown = $.fn.select2.amd.require('select2/dropdown/search');
+// const _renderSearchDropdown = SearchDropdown.prototype.render;
+// SearchDropdown.prototype.render = function () {
+//   const $rendered = _renderSearchDropdown.apply(this, Array.prototype.slice.apply(arguments));
+//   this.$search.attr('placeholder', this.options.get('searchInputPlaceholder'));
+//   return $rendered;
+// };
 
 const textareaCount = () => {
   $('.js-textarea-count').each(function () {
@@ -41,44 +41,44 @@ const formInputFilled = () => {
   });
 };
 
-const customDropdown = () => {
-  $('.js-custom-dropdown').each(function () {
-    const isCountry = $(this).data('country');
-    const placeholder = $(this).data('placeholder') || null;
+// const customDropdown = () => {
+//   $('.js-custom-dropdown').each(function () {
+//     const isCountry = $(this).data('country');
+//     const placeholder = $(this).data('placeholder') || null;
 
-    const options = {
-      width: '100%',
-      dropdownParent: $(this).parent(),
-      placeholder: placeholder,
-    };
+//     const options = {
+//       width: '100%',
+//       dropdownParent: $(this).parent(),
+//       placeholder: placeholder,
+//     };
 
-    const formatCountry = (state) => {
-      if (!state.id) {
-        return state.text;
-      }
-      const baseUrl = '/assets/images/flag/';
-      const $state = $(
-        '<span class="select2-result-flex"><img src="' +
-          baseUrl +
-          state.element.value.replaceAll(' ', '-').toLowerCase() +
-          '.svg" alt="" /> ' +
-          state.text +
-          '</span>',
-      );
-      return $state;
-    };
+//     const formatCountry = (state) => {
+//       if (!state.id) {
+//         return state.text;
+//       }
+//       const baseUrl = '/assets/images/flag/';
+//       const $state = $(
+//         '<span class="select2-result-flex"><img src="' +
+//           baseUrl +
+//           state.element.value.replaceAll(' ', '-').toLowerCase() +
+//           '.svg" alt="" /> ' +
+//           state.text +
+//           '</span>',
+//       );
+//       return $state;
+//     };
 
-    if (isCountry) {
-      options.templateResult = formatCountry;
-      options.templateSelection = formatCountry;
-    }
+//     if (isCountry) {
+//       options.templateResult = formatCountry;
+//       options.templateSelection = formatCountry;
+//     }
 
-    $(this).select2(options);
-  });
-};
+//     $(this).select2(options);
+//   });
+// };
 
 export default function form() {
   textareaCount();
   formInputFilled();
-  customDropdown();
+  // customDropdown();
 }
